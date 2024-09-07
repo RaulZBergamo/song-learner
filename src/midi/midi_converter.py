a02141a4a05327871c8686c6496e2a41cdf7734a
RaulZBergamo <raulbergamo416@gmail.com>
2024-09-06 22:48:12 -0300

    fix(midi note converter)

--- src/midi/midi_converter.py
+++ src/midi/midi_converter.py
@@ -2,6 +2,8 @@
 Módulo para fazer a conversão dos número de pitch MIDI em nomes de notas musicais.
 """
 
+from typing import Dict, Any
+
 class MidiConverter:
     """
     Classe responsável por converter números MIDI em nomes de notas musicais.
@@ -11,17 +13,19 @@ class MidiConverter:
         # Definir as notas padrão
         self.note_names = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
-    def midi_to_note_name(self, midi_number):
+    def midi_to_note_name(self, midi_number: int) -> str:
         """
         Converte um número MIDI em uma nota musical no formato padrão.
         :param midi_number: Número MIDI (int)
         :return: Nome da nota (str)
         """
-        octave = (midi_number // 12) - 1
+        octave = (midi_number // 12)
         note = self.note_names[midi_number % 12]
+        if '-' in note:
+            pass
         return f"{note}{octave}"
 
-    def process_note_data(self, note_data):
+    def process_note_data(self, note_data: Dict[str, Any]) -> str:
         """
         Processa dados de uma nota extraída de um JSON e obtém o nome da nota MIDI.
         :param note_data: Dicionário com informações da nota (ex. pitch)
