d6c58389e55f4bc28459b045cebc2814c329ad75
RaulZBergamo <raulbergamo416@gmail.com>
2024-09-06 22:48:40 -0300

    add(requirements + utils + controllers init)

---
+++ src/utils.py
@@ -0,0 +1,33 @@
+"""
+Módulo com funções utilitárias para o projeto.
+"""
+
+from typing import List
+
+# Dicionário de mapeamento das notas para números base
+note_to_base_label = {
+    'C': 0, 'C#': 1, 'D': 2, 'D#': 3, 'E': 4, 'F': 5,
+    'F#': 6, 'G': 7, 'G#': 8, 'A': 9, 'A#': 10, 'B': 11
+}
+
+def convert_notes_to_labels(notes: List[str]) -> List[int]:
+    """
+    Converte uma lista de notas musicais com oitavas em rótulos numéricos únicos.
+    
+    A fórmula para o rótulo é: (oitava * 12) + número da nota.
+    
+    :param notes: Lista de notas musicais com oitavas (ex: ['C4', 'D#5', 'E3'])
+    :return: Lista de rótulos numéricos únicos correspondentes às notas com oitavas.
+    """
+    labels = []
+    for note in notes:
+        # Separar o nome da nota da oitava (ex: 'C4' -> 'C' e '4')
+        note_name = note[:-1]  # Parte da nota, ex: 'C'
+        octave = int(note[-1])  # Parte da oitava, ex: 4
+
+        # Converter a nota para o rótulo base e ajustar pela oitava
+        note_label = note_to_base_label[note_name]
+        label = (octave * 12) + note_label  # Calcula o rótulo único
+        labels.append(label)
+    
+    return labels
